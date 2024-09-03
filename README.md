60c66c433f7226532b5db04666a96cbd2bc9d557
Wasim Mahmud Surjo <36703903+wasimsurjo@users.noreply.github.com>
2024-09-03 12:10:06 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Info
+
+A smart web screenshot taker that is designed to bypass several issues commonly faced by scapers
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart_web_screenshot",
+    version="1",
+    packages=find_packages(),
+    install_requires=["selenium", "undetected-chromedriver", "requests"],
+    author="Wasim Surjo",
+    author_email="wasimmiller55@gmail.com",
+    description="A smart web screenshot taker that is designed to bypass several issues commonly faced by scapers",
+    url="https://github.com/wasimsurjo/smart_web_screenshot",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
